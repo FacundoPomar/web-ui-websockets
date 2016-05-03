4c5a77c520e091fadca5e00077a72f38c6f17609
Facundo Pomar <Facundo.X.Pomar.-ND@disney.com>
2016-05-03 11:02:39 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var conn = new WebSocket('ws://localhost:9090');
+conn.onopen = function(e) {
+    console.log("Connection established!");
+};
+
+conn.onmessage = function(e) {
+    var data;
+	try {
+		data = JSON.parse(e.data);
+		console.log(data);
+	} catch (err) {
+		data.data = err;
+	}
+    $(".lala").html(data.data);
+};
+
+$("#send").on("click", function () {
+	conn.send($('#mensajero').val());
+});
+
+$(document).ready(function() {
+	// var source   = $("#entry-template").html();
+	// var template = Handlebars.compile(source);
+	// var context = {title: "My New Post", body: "This is my first post!"};
+	// var html    = template(context);
+	// $(".lala").html(html);
+});;
